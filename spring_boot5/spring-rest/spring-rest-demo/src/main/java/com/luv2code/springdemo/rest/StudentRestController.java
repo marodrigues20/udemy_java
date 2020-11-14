c261715ba2ddc7558279204d7b9702be5a8007bf
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2020-11-14 20:20:21 +0000

    spring boot restful

---
+++ spring_boot5/spring-rest/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,56 @@
+package com.luv2code.springdemo.rest;
+
+import com.luv2code.springdemo.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostContractor to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornia","Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students - return list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        
+        return theStudents;
+    }
+
+    // define endpoint for "/students/{students}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // just index into  the list ... keep it simple for now
+
+        // check the studentId against list size
+
+        if( (studentId > theStudents.size()) || (studentId <0 )){
+            throw new StudentNotFoundException("Student Id not found - " + studentId );
+        }
+
+        return theStudents.get(studentId);
+    }
+
+
+
+
+}
---
+++ spring_boot5/spring-rest/spring-rest-demo/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+   <h3> Spring REST Demo </h3>
+
+<hr>
+
+   <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+   <br><br>
+   <a href="${pageContext.request.contextPath}/api/students">Students</a>
+
+
+</body>
+</html>
