c5dd439a2015352cdb6706607b91356640260d36
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2020-11-14 21:04:59 +0000

    spring-and-hiberante modules

---
+++ spring_boot5/spring-and-hibernate/spring-demo-aop-zzz-around-handle-exception/src/main/java/com/luv2code/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,45 @@
+package com.luv2code.aopdemo;
+
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class AfterThrowingDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from spring container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // call method to find the accounts
+        List<Account> theAccounts = null;
+
+        try{
+            // add a boolean flag to simulate exception
+            boolean tripWare = true;
+            theAccountDAO.findAccounts(tripWare);
+        }catch (Exception ex){
+            System.out.println("\n\nMain Program ... caught exception: " + ex);
+        }
+
+
+        // display the accounts
+        System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+        System.out.println("-----");
+
+        System.out.println(theAccounts);
+
+        System.out.println("\n");
+
+        // close the context
+        context.close();
+
+
+    }
+}
