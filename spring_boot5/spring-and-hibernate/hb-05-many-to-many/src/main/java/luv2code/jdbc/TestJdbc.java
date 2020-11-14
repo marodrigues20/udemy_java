d7b8254d6cae16d83881a8c6d1a573a6b71c4d77
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2020-11-14 20:49:43 +0000

    spring-and-hiberante modules

---
+++ spring_boot5/spring-and-hibernate/hb-05-many-to-many/src/main/java/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,33 @@
+package luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+
+        String jdbcUrl = "jdbc:postgresql://localhost/hb-05-many-to-many";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+
+
+        try{
+
+            System.out.println("Connection to database: " + jdbcUrl);
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user,pass);
+
+            System.out.println("We have a successful!!!");
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+}
