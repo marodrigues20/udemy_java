fd5aa1cd13845c3c91920ce414ae4d6e4f20acca
Mario Alexandre <marodrigues20@gmail.com>
2021-08-20 17:29:35 +0100

    Currency-Exchange-Service micro-service

---
+++ microservices-springboot-springcloud/microservices/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.in28minutes.microservices.currencyexchangeservice.repository;
+
+
+import com.in28minutes.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
