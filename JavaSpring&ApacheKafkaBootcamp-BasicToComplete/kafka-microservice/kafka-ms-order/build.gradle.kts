49ecc6b84658e81c0706e2d9b6a40cd682922b90
Mario Alexandre dos Reis Rodrigues <marodrigues20@gmail.com>
2023-04-13 21:38:22 +0100

    50. Setting Up The Project

---
+++ JavaSpring&ApacheKafkaBootcamp-BasicToComplete/kafka-microservice/kafka-ms-order/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.11-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.course.kafka"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springCloudVersion"] = "2021.0.6"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+	implementation("org.springframework.kafka:spring-kafka")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ JavaSpring&ApacheKafkaBootcamp-BasicToComplete/kafka-microservice/kafka-ms-reward/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.11-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.course.kafka"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.kafka:spring-kafka")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
