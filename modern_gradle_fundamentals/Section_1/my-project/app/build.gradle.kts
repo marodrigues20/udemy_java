167f1bb5a67ea55ba43d355bc3402d21ad2b0b79
Mario Alexandre <marodrigues20@gmail.com>
2026-08-23 21:09:31 +0100

    Compose an Application - Add Dependencies

--- modern_gradle_fundamentals/Section_1/my-project/app/build.gradle.kts
+++ modern_gradle_fundamentals/Section_1/my-project/app/build.gradle.kts
@@ -1,4 +1,13 @@
-
 plugins {
     id("my-application")
+}
+
+// Enable Gradle to run the application
+application {
+    mainClass.set("com.example.MyApplication")
+}
+
+dependencies {
+    implementation(project(":data-model"))
+    implementation(project(":business-logic"))
 }
